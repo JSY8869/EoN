659fa3d1f4ee0788fc07c54b57cdbe51f38bb230
JSY8869 <65009713+JSY8869@users.noreply.github.com>
2021-03-10 17:18:10 +0900

    답안 수정
    답안 수정

--- New_HomeWork/4th/Bubble_sort.py
+++ New_HomeWork/4th/Bubble_sort.py
@@ -2,7 +2,5 @@ Sort_List = list(map(int,input().split()))
 for j in range(len(Sort_List)):
     for i in range(0, len(Sort_List)-j-1):
         if(Sort_List[i] > Sort_List[i+1]):
-            temp = Sort_List[i]
-            Sort_List[i] = Sort_List[i+1]
-            Sort_List[i+1] = temp
+            Sort_List[i], Sort_List[i+1] = Sort_List[i+1], Sort_List[i]
 print(Sort_List)
